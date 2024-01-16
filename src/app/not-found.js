336628659916f717f94582a80d27f673f9205307
RAZIBUL HOSSEN <razibulhossen@MacBook-Air-3.local>
2024-01-16 22:28:43 +0100

    first commit

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <h1 className="text-5xl">404!!! Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
